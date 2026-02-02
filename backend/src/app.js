927b459a058cd78036e8dd58bc11c454a50069ce
ShubhamNagar0004 <subhamnagar245@gmail.com>
2026-02-02 21:23:09 +0530

    Authentication of user & learn JWt

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,6 +1,9 @@
 // CREATE SERVER
 const express = require("express");
+const cookieParser = require("cookie-parser");
 const app = express();
+app.use(cookieParser());
+app.use(express.json());
 app.get("/", (req, res) => {
   res.send("server is running on port 3000");
 });
